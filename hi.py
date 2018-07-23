ae7d1ab7cfee431ee09c392b0819178a5d69ab43
developertqw2017 <2144799613@qq.com>
2018-07-23 21:01:52 +0800

    hi fix

--- hi.py
+++ hi.py
@@ -13,34 +13,40 @@ with open(filename_read) as f:
     for row in reader:
         url_value = {'keyword':row[0]}
         row_city = re.findall(r'[（](.*?)[）]', row[3])
-        print(row_city[0])
-        print(row[3])
 	#a = ['（',row_city[0],'）']
         #row_city = ''.join(a)
         #print(row_city)
         city = row[3].replace('（' + row_city[0] + '）','')
-	
-	
         url_value1 = {'city': city}
         url_value = parse.urlencode(url_value)
         url_value1 = parse.urlencode(url_value1)
         url='https://restapi.amap.com/v3/place/text?key=9f9586f7ece479799be36af4f2aa733c&%s&types=&%s&children=1&offset=1&page=1&extensions=all' % (url_value,url_value1)
-        print(url)
+        #print(url)
         data = request.urlopen(url).read()
         data_a = data.decode('UTF-8')
-        print(data_a)
         address = re.findall(r'"address"\:"(.*?)"',data_a)
-        print(address[0])
+        pname = re.findall(r'"pname"\:"(.*?)"',data_a)
+        cityname = re.findall(r'"cityname"\:"(.*?)"',data_a)
+        adname = re.findall(r'"adname"\:"(.*?)"',data_a)
+        location = re.findall(r'"location"\:"(.*?)"',data_a)
+        tel = re.findall(r'"tel"\:"(.*?)"',data_a)
         #data_a = json.loads(data)
+        print(pname[0])
+        print(cityname[0])
+        print(adname[0])
+        print(address[0])
+        print(location[0])
         
         #data_a = data_a['pois']
         write_da.append(data_a)
-        if x%100==0:
-            with open(filename_write,'a') as fw:
-                json.dump(data_a, fw)
-                print("写入json文件，第%s 条" %(x) )
-        x +=1
-        if x < 0:
-            print("此次更新 %s 条" %(x))
-            break
-
+        with open(filename_write,'a') as fw:
+            try:
+                wr = [row[0],',',row[1],',',row[2],',',row[3],',',pname[0],',',pname[0]+cityname[0]+adname[0]+address[0],',',tel[0],',',row[7],',',location[0],'\n']
+                wr1 = ''.join(wr)
+                print(wr1)
+            except:
+                wr = [row[0],',',row[1],',',row[2],',',row[3],',',pname,',',pname+cityname+adname+address,',',tel,',',row,',',location,'\n']
+                print("Error:该医院信息未搜索到")
+                continue
+            else:
+                fw.write(wr1)
