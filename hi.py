2695b7ed2322b5a8fa38e6a8aeb10c22e786a883
developertqw2017 <2144799613@qq.com>
2018-07-24 11:30:28 +0800

    hopitol_data

--- hi.py
+++ hi.py
@@ -11,17 +11,25 @@ with open(filename_read) as f:
     x = 0
     write_da = []
     for row in reader:
-        url_value = {'keyword':row[0]}
+        url_value = {'keywords':row[0]}
         row_city = re.findall(r'[（](.*?)[）]', row[3])
 	#a = ['（',row_city[0],'）']
         #row_city = ''.join(a)
         #print(row_city)
+        #try:
         city = row[3].replace('（' + row_city[0] + '）','')
+        #except:
+        #    print("ERROR 没有找到该城市")
+        #    with open(filename_write, 'a') as fw:
+        #        wr = [row[0],',',row[1],',',row[2],',',row[3],',','',',','',',','',',','',',','','\n']
+        #        wr1 = ''.join(wr)
+        #        fw.write(wr1)
+        #    continue
         url_value1 = {'city': city}
         url_value = parse.urlencode(url_value)
         url_value1 = parse.urlencode(url_value1)
-        url='https://restapi.amap.com/v3/place/text?key=9f9586f7ece479799be36af4f2aa733c&%s&types=&%s&children=1&offset=1&page=1&extensions=all' % (url_value,url_value1)
-        #print(url)
+        url='http://restapi.amap.com/v3/place/text?key=9f9586f7ece479799be36af4f2aa733c&%s&types=&%s&children=1&offset=1&page=1&extensions=base' % (url_value,url_value1)
+        print(url)
         data = request.urlopen(url).read()
         data_a = data.decode('UTF-8')
         address = re.findall(r'"address"\:"(.*?)"',data_a)
@@ -31,12 +39,11 @@ with open(filename_read) as f:
         location = re.findall(r'"location"\:"(.*?)"',data_a)
         tel = re.findall(r'"tel"\:"(.*?)"',data_a)
         #data_a = json.loads(data)
-        print(pname[0])
-        print(cityname[0])
-        print(adname[0])
-        print(address[0])
-        print(location[0])
-        
+        #print(pname[0])
+        #print(cityname[0])
+        #print(adname[0])
+        #print(address[0])
+        #print(location[0])
         #data_a = data_a['pois']
         write_da.append(data_a)
         with open(filename_write,'a') as fw:
@@ -45,8 +52,11 @@ with open(filename_read) as f:
                 wr1 = ''.join(wr)
                 print(wr1)
             except:
-                wr = [row[0],',',row[1],',',row[2],',',row[3],',',pname,',',pname+cityname+adname+address,',',tel,',',row,',',location,'\n']
+        #        wr = [row[0],',',row[1],',',row[2],',',row[3],',',pname,',',pname+cityname+adname+address,',',tel,',',row,',',location,'\n']
+                wr = [row[0],',',row[1],',',row[2],',',row[3],',','',',','',',','',',','',',','','\n']
+                wr1 = ''.join(wr)
                 print("Error:该医院信息未搜索到")
+                fw.write(wr1)
                 continue
-            else:
+            else:        
                 fw.write(wr1)
